1ff8f8b5d23a0e69e49d4e6f23947ab4f6907653
handika <handikacoco@gmail.com>
2021-08-27 14:43:21 +0700

    add foreign key to notes owner column

--- migrations/1629021023094_add-column-owner-to-table-notes.js
+++ migrations/1629021023094_add-column-owner-to-table-notes.js
@@ -2,14 +2,14 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {
-	pgm.addColumn('notes', {
-		owner: {
-			type: 'VARCHAR(50)'
-		}
-	})
+exports.up = (pgm) => {
+  pgm.addColumn('notes', {
+    owner: {
+      type: 'VARCHAR(50)',
+    },
+  });
 };
 
-exports.down = pgm => {
-	pgm.dropColumn('notes', 'owner')
+exports.down = (pgm) => {
+  pgm.dropColumn('notes', 'owner');
 };
