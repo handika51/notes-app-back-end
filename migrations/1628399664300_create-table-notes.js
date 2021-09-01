b75f4ed03bb0d47d79809f8524e3a3d9c297e8ed
handika <handikacoco@gmail.com>
2021-09-01 15:51:00 +0700

    export notes implemented

--- migrations/1628399664300_create-table-notes.js
+++ migrations/1628399664300_create-table-notes.js
@@ -1,36 +1,36 @@
 /* eslint-disable camelcase */
 
-exports.shorthands = undefined;
+exports.shorthands = undefined
 
 exports.up = (pgm) => {
   pgm.createTable('notes', {
     id: {
       type: 'VARCHAR(50)',
-      primaryKey: true,
+      primaryKey: true
     },
     title: {
       type: 'TEXT',
-      notNull: true,
+      notNull: true
     },
     body: {
       type: 'TEXT',
-      notNull: true,
+      notNull: true
     },
     tags: {
       type: 'TEXT[]',
-      notNull: true,
+      notNull: true
     },
     created_at: {
       type: 'TEXT',
-      notNull: true,
+      notNull: true
     },
     updated_at: {
       type: 'TEXT',
-      notNull: true,
-    },
-  });
-};
+      notNull: true
+    }
+  })
+}
 
 exports.down = (pgm) => {
-  pgm.dropTable('notes');
-};
+  pgm.dropTable('notes')
+}
