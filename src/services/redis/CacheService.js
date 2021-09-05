06a7787e9f6fecc170c961ef1c9a76b397c69d6c
handika <handikacoco@gmail.com>
2021-09-05 23:27:56 +0700

    add cache

---
+++ src/services/redis/CacheService.js
@@ -0,0 +1,53 @@
+const redis = require('redis')
+
+class CacheService {
+  constructor () {
+    this._client = redis.createClient({
+      host: process.env.REDIS_SERVER
+    })
+
+    this._client.on('error', (error) => {
+      console.log(error)
+    })
+  }
+
+  set (key, value) {
+    return new Promise((resolve, reject) => {
+      this._client.set(key, value, (error, ok) => {
+        if (error) {
+          return reject(error)
+        }
+        return resolve(ok)
+      })
+    })
+  }
+
+  get (key) {
+    return new Promise((resolve, reject) => {
+      this._client.get(key, (error, reply) => {
+        if (error) {
+          return reject(error)
+        }
+
+        if (reply === null) {
+          return reject(new Error('Cache tidak ditemukan'))
+        }
+
+        return resolve(reply.toString())
+      })
+    })
+  }
+
+  delete (key) {
+    return new Promise((resolve, reject) => {
+      this._client.del(key, (error, count) => {
+        if (error) {
+          return reject(error)
+        }
+        return resolve(count)
+      })
+    })
+  }
+}
+
+module.exports = CacheService
